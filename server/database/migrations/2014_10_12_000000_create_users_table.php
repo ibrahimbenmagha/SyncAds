73473bfcbd5f831d02f7532aa379b7eb0427c463
ibrahim <ibrahimbenmagha16@gmail.com>
2024-02-03 10:09:57 +0100

    add some edits to  report

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('UserName');
+            $table->string('UserLName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string("telNumber")->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
